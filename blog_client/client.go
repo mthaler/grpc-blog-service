a78e029d60bac01951ea4b55cdb86d4af6de8010
Michael Thaler <michael.thaler@dlr.de>
2021-05-07 13:10:17 +0200

    Added robo3t.jpg.

--- blog_client/client.go
+++ blog_client/client.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/mthaler/grpc-blog-service/blogpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
 func main() {
 
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close() // Maybe this should be in a separate function and the error handled?
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	// create Blog
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Michael",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has been created: %v", createBlogRes)
+	blogID := createBlogRes.GetBlog().GetId()
+
+	fmt.Println(blogID)
 }
