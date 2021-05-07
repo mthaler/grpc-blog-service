2aa930efb871d2e4ada6a62e92ff2878d646422b
Michael Thaler <michael.thaler@dlr.de>
2021-05-07 11:37:59 +0200

    Started to implement server.go.

--- blog_server/server.go
+++ blog_server/server.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mthaler/grpc-blog-service/blogpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+	fmt.Println("Starting calculator service...")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
